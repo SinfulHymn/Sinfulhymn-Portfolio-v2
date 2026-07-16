e4f2534e6065aa8bc762c873f059ec42a89894bf
Sinfulhymn <kmachappy@pm.me>
2026-07-16 00:52:58 -0700

    all

---
+++ data/headerNavLinks.js
@@ -0,0 +1,7 @@
+const headerNavLinks = [
+  { href: '/about', title: 'About' },
+  { href: '/blog', title: 'Blog' },
+  { href: '/projects', title: 'Projects' },
+]
+
+export default headerNavLinks
---
+++ pages/projects.js
@@ -0,0 +1,37 @@
+import siteMetadata from '@/data/siteMetadata'
+import projectsData from '@/data/projectsData'
+import Card from '@/components/Card'
+import { PageSEO } from '@/components/SEO'
+
+export default function Projects() {
+  return (
+    <>
+      <PageSEO title={`Projects - ${siteMetadata.author}`} description={siteMetadata.description} />
+      <div className="">
+        <div className="space-y-2 py-4 md:space-y-5">
+          <h1 className="text-2xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-2xl sm:leading-10 md:text-3xl md:leading-5">
+            Projects
+          </h1>
+          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
+            Web Development Projects
+          </p>
+        </div>
+        <div className="container py-12">
+          <div className="grid grid-cols-1 gap-4 md:grid-cols-2 ">
+            {projectsData.map((d) => (
+              <Card
+                key={d.title}
+                title={d.title}
+                description={d.description}
+                imgSrc={d.imgSrc}
+                href={d.href}
+                repo={d.repo}
+                tags={d.tags}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
